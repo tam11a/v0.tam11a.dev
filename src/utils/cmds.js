cdb5d84a0f8d6090721c3959fdebf284c77c44c1
tam11a <ibrahimsadiktamim@gmail.com>
2022-11-17 07:41:10 +0600

    updated: export

--- src/utils/cmds.js
+++ src/utils/cmds.js
@@ -1,4 +1,4 @@
-exports.cmdlist = {
+export const cmdlist = {
   lastSession: (ip, ts) => {
     return ts && ip
       ? `Welcome to Tam.ENV 11.a.0.1 LTS (GNU/Linux 5.4.0-128-generic x86_64)
@@ -18,30 +18,30 @@ Last login: ${ts} from ${ip}
   },
   cd: (path, currentDir) => {
     let x;
-    if (!this.explore(path)) {
-      x = this.getLS(currentDir);
+    if (!explore(path)) {
+      x = getLS(currentDir);
     } else {
-      if (this.explore(path)[0] === "root") x = this.getLS(path);
+      if (explore(path)[0] === "root") x = getLS(path);
       else {
-        x = this.getLS([...this.explore(currentDir), ...this.explore(path)]);
+        x = getLS([...explore(currentDir), ...explore(path)]);
       }
     }
     if (x.startsWith("bash:")) return x;
     else return "";
   },
   ls: (path, currentDir) => {
-    if (!this.explore(path)) {
-      return this.getLS(currentDir);
+    if (!explore(path)) {
+      return getLS(currentDir);
     } else {
-      if (this.explore(path)[0] === "root") return this.getLS(path);
+      if (explore(path)[0] === "root") return getLS(path);
       else {
-        return this.getLS([...this.explore(currentDir), ...this.explore(path)]);
+        return getLS([...explore(currentDir), ...explore(path)]);
       }
     }
   },
 };
 
-exports.explore = (path) => {
+export const explore = (path) => {
   if (path?.[path?.length - 1] === "/")
     path = path.slice(0, path[path?.length - 1]);
   path = path?.split("/");
@@ -49,10 +49,10 @@ exports.explore = (path) => {
   return path;
 };
 
-exports.getLS = (path) => {
-  let tempList = this.arrangeLS(
-    exports.folders,
-    typeof path === "string" ? this.explore(path) : path || []
+export const getLS = (path) => {
+  let tempList = arrangeLS(
+    folders,
+    typeof path === "string" ? explore(path) : path || []
   );
   if (!tempList)
     return `bash: cannot access '${
@@ -71,9 +71,9 @@ exports.getLS = (path) => {
   return xfiles.trim();
 };
 
-exports.arrangeLS = (directory, newList) => {
+export const arrangeLS = (directory, newList) => {
   if (!newList.length) return directory;
-  return this.arrangeLS(
+  return arrangeLS(
     directory?.[newList[0]],
     newList[0] === "ls"
       ? newList.slice(1)
@@ -83,7 +83,7 @@ exports.arrangeLS = (directory, newList) => {
   );
 };
 
-exports.folders = {
+export const folders = {
   root: {
     ls: {
       about: {
